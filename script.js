8b4e0c8d0a5d03c991341bb104c2f1f8040f0f5b
CourtSteph17 <courtsteph17@gmail.com>
2026-06-25 01:09:32 -0500

    Updated script.js and added a new funciton for user input

--- script.js
+++ script.js
@@ -4,14 +4,18 @@ function getComputerChoice() {
     const result = Math.floor(Math.random() * 3);
     switch (result) {
         case 0 :
-            return console.log("rock");
+            return console.log("Rock");
         case 1 :
-            return console.log("paper");
+            return console.log("Paper");
         case 2 :
-            return console.log("scissors");
+            return console.log("Scissors");
     }
 }
 
-
+function getHumanChoice() {
+    const choice = prompt("Rock, Paper, or Scissors?");
+    return choice;
+}
 
 getComputerChoice();
+getHumanChoice();
