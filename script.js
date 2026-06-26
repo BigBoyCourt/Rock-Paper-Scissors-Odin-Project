6f77ce34ac709de77ed036179572f58db6944bf3
CourtSteph17 <courtsteph17@gmail.com>
2026-06-26 01:06:29 -0500

    Updated script.js added functionality to playRound Created playGame function in script.js Updated playGame function with a game loop and game result
    playRound function now has tie, win, loss versus computer and player
    playGame function created in script.js for game loop
        game loop has iteration that recalls playRound five times and decided winner by comparing humanScore with computerScore, which is added after every round besides ties.

--- script.js
+++ script.js
@@ -1,21 +1,56 @@
 console.log("Hello World!");
 
+let humanScore = 0;
+let computerScore = 0;
+
+
+
+
+
+function playRound(humanSelection, computerSelection){
+    if (humanSelection === computerSelection) {
+        return console.log(`It's a tie this round! | Human Score: ${humanScore} | Computer Score: ${computerScore}`);
+    } else if (
+        (humanSelection === "rock" && computerSelection === "scissors") ||
+        (humanSelection === "paper" && computerSelection === "rock") ||
+        (humanSelection === "scissors" && computerSelection === "paper")
+    ) {
+        humanScore++;
+        return console.log(`You won this round, ${humanSelection} beats ${computerSelection}! | Human Score: ${humanScore} | Computer Score: ${computerScore}`);
+    } else {
+        computerScore++;
+        return console.log(`You lost this round, ${computerSelection} beats ${humanSelection}! | Human Score: ${humanScore} | Computer Score: ${computerScore}`);
+    }
+}
+
 function getComputerChoice() {
     const result = Math.floor(Math.random() * 3);
     switch (result) {
-        case 0 :
-            return console.log("Rock");
-        case 1 :
-            return console.log("Paper");
-        case 2 :
-            return console.log("Scissors");
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissors";
     }
 }
 
 function getHumanChoice() {
-    const choice = prompt("Rock, Paper, or Scissors?");
-    return choice;
+    const choice = prompt(`Rock, Paper, or Scissors?`);
+    return choice ? choice.toLowerCase() : "";
 }
 
-getComputerChoice();
-getHumanChoice();
+function playGame(playRound) {
+    for (let i = 0; i < 5; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+    }
+    if (humanScore > computerScore) {
+        console.log(`You won the game!`)
+    } else if (humanScore < computerScore) {
+        console.log(`You lost the game!`)
+    } else console.log(`The game is a tie!`)
+}
+
+playGame(playRound);
